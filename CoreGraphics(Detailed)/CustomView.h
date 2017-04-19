ebaf732c71adbfdfa771ef281a22dbf843d9fa10
王红昌 <Dustin@wanghongchangdeMacBook-Pro.local>
2017-04-19 22:38:43 +0800

    first commit

---
+++ CoreGraphics(Detailed)/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  CoreGraphics(Detailed)
+//
+//  Created by 王红昌 on 17/4/19.
+//  Copyright © 2017年 王红昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
---
+++ CoreGraphics(Detailed)/CustomView.m
@@ -0,0 +1,21 @@
+//
+//  CustomView.m
+//  CoreGraphics(Detailed)
+//
+//  Created by 王红昌 on 17/4/19.
+//  Copyright © 2017年 王红昌. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
